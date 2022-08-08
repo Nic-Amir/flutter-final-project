9a2799931ffc9e198134bf0104b809b8a4ffe9d3
Nic Amir <amir@besquare.com.my>
2022-08-08 17:06:16 +0800

    update service

--- lib/pages/pokemon_detail.dart
+++ lib/pages/pokemon_detail.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_project_pokemon_codex/states/pokemon_cubit.dart';
 
+class PokemonDetail extends StatelessWidget {
+  const PokemonDetail({required this.pokeId, Key? key}) : super(key: key);
+
+  final String pokeId;
+  
+  @override
+  Widget build(BuildContext context) {
+    IdPokemonCubit cubit = BlocProvider.of<IdPokemonCubit>(context)..fetchPokemonById(pokeId)
+
+    return Scaffold()
+  }
+}
