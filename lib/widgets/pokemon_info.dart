841bf5d389bc2f8bd79a1723e58f31e9be10ec46
Vern <vern.yi@besquare.com.my>
2022-08-08 15:47:34 +0800

    add pokemon info widget

---
+++ lib/widgets/pokemon_info.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class PokemonInfomation extends StatelessWidget {
+  const PokemonInfomation({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
