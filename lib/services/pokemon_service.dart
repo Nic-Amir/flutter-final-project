f4c8b40fd9f49e914bcc93d61aa91fe2fa9b83f4
Vern <vern.yi@besquare.com.my>
2022-08-08 17:54:30 +0800

    simply push

--- lib/services/pokemon_service.dart
+++ lib/services/pokemon_service.dart
@@ -5,24 +5,8 @@ class PokemonService {
   Future<PokemonModel> fetchPokeInfoById(String pokeId) async {
     final Uri pokeUrl = Uri(
         scheme: 'https',
-        host: 'https://pokeapi.co/',
-        path: 'api/v2/pokemon/',
-        fragment: pokeId);
-
-    http.Response response = await http.get(pokeUrl);
-
-    if (response.statusCode == 200) {
-      return PokemonModel.fromJson(response.body);
-    } else {
-      throw Exception('error error error please try again');
-    }
-  }
-
-  Future<PokemonModel> fetchPokeInfo() async {
-    final Uri pokeUrl = Uri(
-      scheme: 'https',
-      host: 'https://pokeapi.co/',
-      path: 'api/v2/pokemon/',
+        host: 'pokeapi.co',
+        path: 'api/v2/pokemon/$pokeId',
     );
 
     http.Response response = await http.get(pokeUrl);
@@ -33,4 +17,5 @@ class PokemonService {
       throw Exception('error error error please try again');
     }
   }
+
 }
