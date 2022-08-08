eb338f7e59f51ceda1abda654948459393b36428
Vern <vern.yi@besquare.com.my>
2022-08-08 17:48:21 +0800

    fix smtg

--- lib/services/pokemon_service.dart
+++ lib/services/pokemon_service.dart
@@ -4,26 +4,9 @@ import 'package:http/http.dart' as http;
 class PokemonService {
   Future<PokemonModel> fetchPokeInfoById(String pokeId) async {
     final Uri pokeUrl = Uri(
-        scheme: 'https',
-        host: 'https://pokeapi.co/',
-        path: 'api/v2/pokemon/',
-        fragment: pokeId);
-
-    http.Response response = await http.get(pokeUrl);
-
-    if (response.statusCode == 200) {
-      return PokemonModel.fromJson(response.body);
-    } else {
-      throw Exception('error error error please try again');
-    }
-  }
-
-  Future<PokemonModel> fetchPokeInfo() async {
-    final Uri pokeUrl = Uri(
       scheme: 'https',
-      host: 'https://pokeapi.co/',
-      path: 'api/v2/pokemon/',
-    );
+      host: 'pokeapi.co',
+      path: 'api/v2/pokemon/$pokeId',);
 
     http.Response response = await http.get(pokeUrl);
 
