881a34c01b7e18f897f53aca00c66772aceceeec
Nic Amir <amir@besquare.com.my>
2022-08-09 16:47:31 +0800

    revoke bug

--- lib/widgets/pokemon_card.dart
+++ lib/widgets/pokemon_card.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import '../models/pokemons_model.dart';
 
 class PokemonCard extends StatelessWidget {
-  const PokemonCard({required this.index,required this.pokemonsModel,Key? key}) : super(key: key);
+  const PokemonCard(
+      {required this.index, required this.pokemonsModel, Key? key})
+      : super(key: key);
   final PokemonsModel pokemonsModel;
   final int index;
 
@@ -11,33 +13,36 @@ class PokemonCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: GridTile(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Image.network(
-              'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index +
-                  1}.png',
-              height: 80,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.network(
-                  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${index +
-                      1}.gif',
-                  height: 20,),
-                SizedBox(
-                  width: 5,
-                ),
-                Text(
-                  '${pokemonsModel.results[index].name}',
-                  style: TextStyle(
-                    fontSize: 17
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context)
+                .pushNamed('/poke-details', arguments: (index + 1).toString());
+          },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.network(
+                'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index + 1}.png',
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.network(
+                    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${index + 1}.gif',
+                    height: 20,
                   ),
-                ),
-              ],
-            ),
-          ],
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    '${pokemonsModel.results[index].name}',
+                    style: TextStyle(fontSize: 17),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
