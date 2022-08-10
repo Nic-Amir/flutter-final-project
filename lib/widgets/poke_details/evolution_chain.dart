3ee86c8f7a01e09fd49c5690af832c9778fb4c54
Vern <vern.yi@besquare.com.my>
2022-08-10 16:33:57 +0800

    added stuffs'

---
+++ lib/widgets/poke_details/evolution_chain.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project_pokemon_codex/models/evolution_model.dart';
+
+class EvolutionChain extends StatelessWidget {
+  const EvolutionChain({required this.evolutionModel, Key? key}) : super(key: key);
+  final EvolutionModel evolutionModel;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
