160603dd0b167ad5090b8c36aece18befcfe9a9b
Ng Shin Cheng <shin.cheng@besquare.com.my>
2022-08-11 15:26:39 +0800

    edit home page ui

--- lib/widgets/pokemon_item_info.dart
+++ lib/widgets/pokemon_item_info.dart
@@ -11,17 +11,32 @@ class ItemInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: Colors.red,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+        // mainAxisAlignment: MainAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-
           Container(
             width: MediaQuery.of(context).size.width,
             child: Image.network(
-              'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${itemModel.name}.png',scale: 0.5,),
+              'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${itemModel.name}.png',scale: 0.3,),
             ),
-          Text('${itemModel.names[7].name}'),
+          SizedBox(
+              width: 200,
+              child: Text('${itemModel.names[7].name}')),
+          Row(
+            children: [
+              SizedBox(
+                  // width: 900,
+                  child: Text("${itemModel.flavorTextEntries.first.text}")),
+            ],
+          ),
+          Text(itemModel.effectEntries.first.effect),
+          Text(itemModel.effectEntries.first.shortEffect),
+          Text(itemModel.category.name),
 
         ],
       ),
