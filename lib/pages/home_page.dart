b89a5788a491334c811ddae890a245560daa831a
Nic Amir <amir@besquare.com.my>
2022-08-11 16:13:07 +0800

    add cloud animation at homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -7,30 +8,49 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation _cloudAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 5500));
+
+    _animationController.addListener(() => setState(() {}));
+    _animationController.repeat(reverse: false);
+
+    _cloudAnimation =
+        Tween<Offset>(begin: Offset(400.0, 00), end: Offset(-400.0, 00))
+            .animate(_animationController);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pokedex'),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-
           children: [
-            Stack(
-              alignment: Alignment.topCenter,
-              children: [
-                Image.asset('assets/pokemon-logo.png', height: 400, width: 400),
-                Image.asset('assets/pokemon_cloud.png', scale: 3,),
-
-        ]
+            Stack(alignment: Alignment.topCenter, children: [
+              Image.asset('assets/pokemon-logo.png', height: 400, width: 400),
+              Transform.translate(
+                offset: _cloudAnimation.value,
+                child: Image.asset(
+                  'assets/pokemon_cloud.png',
+                  scale: 1,
                 ),
-
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
+              ),
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
               ElevatedButton(
                   onPressed: () {
                     Navigator.of(context).pushNamed('/poke-list');
